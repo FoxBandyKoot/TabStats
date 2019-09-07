9b4a74d081546224d953aeb34cbb95cf5f42dc3c
FoxBandyKoot <lerenard.charly@gmail.com>
2019-09-07 21:51:43 +0200

    threads on F2 do not get combin list, this function has moved to now file

--- LogiquePython/ScriptTabStats_Fast_Tests.py
+++ LogiquePython/ScriptTabStats_Fast_Tests.py
@@ -14,15 +14,12 @@ class TabStats():
     script = ''
     pathXLSX = ''
     full_table = list()
-    employees = list()
-    nb_columns = 0
-    nb_lines = 0
     index_best_group = 0
     
     '''
     TO CHANGE
     ''' 
-    object_per_group = 5           # Object per combin
+    object_per_group = 2           # Object per combin
 
     F1_run = False
     F2_run = True
@@ -50,13 +47,13 @@ class TabStats():
         worksheet = self.excelFileSelection(self)
 
         self.full_table, employees = self.get_header_employees(self, worksheet)
-        [self.nb_lines, self.nb_columns] = shape(self.full_table)
+        [nb_lines, nb_columns] = shape(self.full_table)
 
         if self.F1_run:
-            self.F1_go(self, employees)
+            self.F1_go(self, employees, nb_lines)
 
         elif self.F2_run:
-            self.F2_go(self, employees)
+            self.F2_go(self, employees, nb_lines)
 
 
     def excelFileSelection(self):
@@ -97,61 +94,16 @@ class TabStats():
         return(main_list, employees)    
 
 
-    def F1_go(self, employees):
+    def F1_go(self, employees, nb_lines):
         f1 = F1()
-        f1.init(self.selectedEmployees, employees, self.nb_lines, self.full_table)
+        f1.init(self.selectedEmployees, employees, nb_lines, self.full_table)
 
 
-    def F2_go(self, employees):
-        # Boundaries for each threads
-        if self.nb_lines % 2 == 0:
-            first_half_lines = self.nb_lines / 2
-            second_half_lines = self.nb_lines
-        else:
-            print("FUCK IT")
-            
-        # Create threads
-        th_first_half_tab = F2("first_half_tab", first_half_lines, employees, self.full_table, self.object_per_group)
-        th_second_half_tab = F2("second_half_tab", second_half_lines, employees, self.full_table, self.object_per_group)
-
-        # Run threads
-        th_first_half_tab.start()
-        th_second_half_tab.start()
-
-        # Wait threads to finish
-        th_first_half_tab.join()
-        th_second_half_tab.join()
-
-        # Convert list to int
-        th_first_half_tab.value_each_group = list(map(int, th_first_half_tab.value_each_group))
-        th_second_half_tab.value_each_group = list(map(int, th_second_half_tab.value_each_group))
+    def F2_go(self, employees, nb_lines):
         
-        
-        # Merge the 2 threads
-        value_each_group = [x + y for x, y in zip(th_first_half_tab.value_each_group, th_second_half_tab.value_each_group)]
-        
-        # Get index of the best group
-        self.index_best_group, maxi = self.maximum(self, value_each_group)
-        '''
-        print(th_first_half_tab.combin_list)
-        '''
-        if th_first_half_tab.combin_list:
-            self.best_group = th_first_half_tab.combin_list[self.index_best_group]
-            print("best_group")
-            print(self.best_group)
-
+        f2 = F2(employees, nb_lines, self.full_table, self.object_per_group)
+        f2.main()
     
-    def maximum(self, value_each_group):
-        '''
-        Return the index of the group of employees which worked the most together
-        '''
-        maxi = max(value_each_group)
-        print("maxi")
-        print(maxi)
-        # Get index of best group
-        index_best_group = value_each_group.index(maxi)
-
-        return(index_best_group, maxi)
 
 '''
 END CLASS
--- LogiquePython/project_sources/F2.py
+++ LogiquePython/project_sources/F2.py
@@ -1,56 +1,118 @@
-from threading import Thread
+from .F2_Threads_part import F2_Threads_part
 
-class F2(Thread):
+class F2():
 
     #dt_name for Dictionnary
-
+    '''
     def __init__(self, half_tab_i, lines_to_read_i, employees_i, full_table_i, object_per_group_i):
-        '''
-        Constructor for threads
-        '''
-        Thread.__init__(self)
-        # Boundaries for threads
-        if half_tab_i == "first_half_tab":
-            self.iterator_line = 0
-        elif half_tab_i == "second_half_tab":
-            self.iterator_line = int(lines_to_read_i / 2)
+    '''
+    #Constructor for threads
+    '''
+    Thread.__init__(self)
+    # Boundaries for threads
+    if half_tab_i == "first_half_tab":
+        self.iterator_line = 0
+    elif half_tab_i == "second_half_tab":
+        self.iterator_line = int(lines_to_read_i / 2)
 
-        self.lines_to_read = int(lines_to_read_i)
+    self.lines_to_read = int(lines_to_read_i)
 
-        self.employees = employees_i        # Header employees
-        self.full_table = full_table_i      # All data
+    self.employees = employees_i        # Header employees
+    self.full_table = full_table_i      # All data
+    self.object_per_group = object_per_group_i
+
+    # All groups
+    self.combin_list = list()
+    self.value_each_group = list()
+
+    self.main()         # get max for each group for one half of the tab per thread
+    '''
+
+    def __init__(self, employees_i, nb_lines_i, full_table_i, object_per_group_i):
+        '''
+        Constructor
+        '''
+        self.employees = employees_i
+        self. nb_lines = nb_lines_i
         self.object_per_group = object_per_group_i
-
-        # All groups
-        self.combin_list = list()
-        self.value_each_group = list()
-
-        self.main()         # get max for each group for one half of the tab per thread
+        self.full_table = full_table_i
 
     def main(self):
         '''
         Gives the most present group at the same time
         Arg 1 : Header tab
         '''
-
         # Get all combinations of all groups
-        self.combin_list = self.get_combin_list(self.employees, self.object_per_group)
-        len_combin_list = len(self.combin_list)
-        #print("len_combin_list")
-        #print(len_combin_list)
-        #self.dt_id_group = self.get_dt_id_group(self.combin_list, len_combin_list)         USELESS FOR NOW
+        combin_list = self.get_combin_list(self.employees, self.object_per_group)
+        len_combin_list = len(combin_list)
+        print("len_combin_list")
+        print(len_combin_list)
+        #self.dt_id_group = self.get_dt_id_group(combin_list, len_combin_list)         USELESS FOR NOW
         
+        '''
         # REPLACE BY list() IF IT IS MORE FAST OR TO HAVE SAME CODE EVERYWHERE
         result = list()
 
         for i in range (len_combin_list):
-            self.value_each_group.append(self.Tabstats_F2(result, self.combin_list[i]))
+            self.value_each_group.append(self.Tabstats_F2(result, combin_list[i]))
+        '''
+        
+        # Boundaries for each threads
+        if self.nb_lines % 2 == 0:
+            first_half_lines = self.nb_lines / 2
+            second_half_lines = self.nb_lines
+        else:
+            print("FUCK IT")
             
+        
+        # Create threads
+        th_first_half_tab = F2_Threads_part("first_half_tab", first_half_lines, self.employees, self.full_table, self.object_per_group, combin_list)
+        th_second_half_tab = F2_Threads_part("second_half_tab", second_half_lines, self.employees, self.full_table, self.object_per_group, combin_list)
+
+        # Run threads
+        th_first_half_tab.start()
+        th_second_half_tab.start()
+
+        # Wait threads to finish
+        th_first_half_tab.join()
+        th_second_half_tab.join()
+
+        # Convert list to int
+        th_first_half_tab.value_each_group = list(map(int, th_first_half_tab.value_each_group))
+        th_second_half_tab.value_each_group = list(map(int, th_second_half_tab.value_each_group))
+        
+        
+        # Merge the 2 threads
+        value_each_group = [x + y for x, y in zip(th_first_half_tab.value_each_group, th_second_half_tab.value_each_group)]
+        
+        # Get index of the best group
+        self.index_best_group, maxi = self.maximum(value_each_group)
+        '''
+        #print(th_first_half_tab.combin_list)
+        '''
+        if th_first_half_tab.combin_list:
+            self.best_group = th_first_half_tab.combin_list[self.index_best_group]
+            print("best_group")
+            print(self.best_group)
+
+    
+    def maximum(self, value_each_group):
+        '''
+        #Return the index of the group of employees which worked the most together
+        '''
+        maxi = max(value_each_group)
+        print("maxi")
+        print(maxi)
+        # Get index of best group
+        index_best_group = value_each_group.index(maxi)
+
+        return(index_best_group, maxi)
+    
 
     def get_combin_list(self, seq, k):
         '''
-        Give all combinations of X desired employees.
-        ">>" is a binary operation : x * 2**y
+        #Give all combinations of X desired employees.
+        #">>" is a binary operation : x * 2**y
         '''
         # REPLACE BY list() IF IT IS MORE FAST OR TO HAVE SAME CODE EVERYWHERE
         p = []
@@ -77,78 +139,3 @@ class F2(Thread):
         for i in range(len_combin_list):
             dt_id_group[i] = combin_list[i]
         return dt_id_group
-
-
-    def Tabstats_F2(self, result, selectedGroup):
-        '''
-        arg2 : group of the combin list
-        Second functionnality : Checks input, do calcul, and return result
-        '''            
-        # Replaces names with numeric values
-        #print(selectedGroup)
-        selectedFigures = self.replaceNamesByNumbers(selectedGroup)
-        # Sorting in ascending order employees, USELESS HERE IN THINK
-        selectedFiguresSorted = self.orderList(selectedFigures)
-        
-        #Count how many time employees selected gave worked together
-        result = self.how_much_worked_together(selectedFiguresSorted)
-        #result = self.how_much_worked_together(selectedFigures)
-        return(result)
-
-
-    def replaceNamesByNumbers(self, selectedGroup):
-        '''
-        Replaces names with their index
-        ARG1: User input, ARG2: Table header
-        '''
-        L = []                
-        for i in range (len(selectedGroup)):
-            for j in range (len(self.employees)):
-                if selectedGroup[i] == self.employees[j]:
-                    L += [j]
-        return(L)
-
-
-    def orderList(self, selectedGroup):
-        # USELESS FUNCTION I THINK 
-        #       print("orderList")
-        #       print(selectedGroup)
-        # AND PRINT JUST AFTER LINE "selectedFigures = self.replaceNamesByNumbers(selectedGroup, self.employees)"
-        #       print("selectedFigures")
-        #       print(selectedFigures)
-
-        '''
-        Sorting employees in ascending order
-        ARG1: User input
-        '''
-        L=[]
-        while selectedGroup!=[]:
-            minim=selectedGroup[0]
-            indice=0
-            for i in range(len(selectedGroup)):
-                if selectedGroup[i] < minim:
-                    minim = selectedGroup[i]
-                    indice = i
-            L+=[selectedGroup[indice]]
-            selectedGroup = selectedGroup[0:indice] + selectedGroup[indice + 1:len(selectedGroup)]
-        return(L)
-
-
-    def how_much_worked_together(self, selectedFiguresSorted):
-        '''
-        Count how many time employees selected gave worked together. Depends on threads 
-        '''
-        '''
-        print(selectedFiguresSorted)
-        '''
-        total = 0
-        for i in range (self.lines_to_read)[self.iterator_line:]:
-            counter = 0
-            for j in range (self.object_per_group):
-                if self.full_table[i] [selectedFiguresSorted[j]] != "":
-                    counter += 1
-            if counter == self.object_per_group:
-                total += 1
-
-        total = str(total)
-        return(total)
